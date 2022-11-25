aa23a60b01d7d873234b61153b4bd0add1aa2a3d
Samurais-Holiday <102521877+Samurais-Holiday@users.noreply.github.com>
2022-11-25 21:00:49 +0900

    local_auth: USE_BIOMETRICのアクセス許可を追加

--- android/app/src/main/kotlin/samurais/holiday/password_manager/MainActivity.kt
+++ android/app/src/main/kotlin/samurais/holiday/password_manager/MainActivity.kt
@@ -1,6 +1,6 @@
 package samurais.holiday.password_manager
 
-import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.android.FlutterFragmentActivity;
 
-class MainActivity: FlutterActivity() {
+class MainActivity: FlutterFragmentActivity() {
 }
